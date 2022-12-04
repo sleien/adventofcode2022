61c8ad8b0cbf9fe50acdcd6fd443b73c74298f59
Noah Schneider <noah.schneider@outlook.com>
2022-12-04 10:28:48 +0100

    added day 4

---
+++ 4/r4_b.py
@@ -0,0 +1,30 @@
+# get all input from input.txt
+
+def main():
+    # open file
+    with open('input.txt', 'r') as f:
+        # read all lines
+        lines = f.readlines()
+        # remove new line characters
+        lines = [line.strip() for line in lines]
+        #split lines by coma
+        lines = [line.replace(',', '-').split('-') for line in lines]
+        # init sum
+        sum = 0
+        # loop through lines
+        for line in lines:
+            line[0] = int(line[0])
+            line[1] = int(line[1])
+            line[2] = int(line[2])
+            line[3] = int(line[3])
+            if (line[0] >= line[2] and line[0] <= line[3]) or (line[2] >= line[0] and line[2] <= line[1]):
+                print(line[0], "-", line[1], "in", line[2], "-", line[3])
+                sum += 1
+            elif (line[0] <= line[2] and line[0] >= line[3]) or (line[2] <= line[0] and line[2] >= line[1]):
+                print(line[0], "-", line[1], "over", line[2], "-", line[3])
+                sum += 1
+        print("Sum: " + str(sum))
+
+
+if __name__ == '__main__':
+    main()
