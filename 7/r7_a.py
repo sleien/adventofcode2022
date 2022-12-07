8170aa1abe4be4c8a94794b97e9538a7f2856dfe
Noah Schneider <noah.schneider@outlook.com>
2022-12-07 08:05:47 +0100

    added day 7

---
+++ 7/r7_a.py
@@ -0,0 +1,76 @@
+# get all input from input.txt
+
+def main():
+    # open file
+    with open('input.txt', 'r') as f:
+        # read all lines
+        lines = f.readlines()
+        # remove new line characters
+        lines = [line.strip() for line in lines]
+        dir_path = "/"
+        system = []
+        for line in lines:
+            if line.startswith("$ cd"):
+                # get the directory to change to
+                dir = line.split(" ")[2]
+                # if dir is a relative path
+                if dir.startswith(".."):
+                    # get the number of directories to go back
+                    dir_path = dir_path[:dir_path.rfind("/")]
+                    if dir_path == "":
+                        dir_path = "/"
+                # if dir is a relative path
+                elif dir.startswith("/"):
+                    # set the system to the root directory
+                    dir_path = ""
+                # if dir is a relative path
+                else:
+                    # add the directory to the system
+                    if not dir_path.endswith("/"):
+                        dir_path += "/"
+                    dir_path += dir
+            elif line[0].isdigit():
+                # get the file name
+                file = line.split(" ")
+                # add the file to the system
+                system.append(dir_path  + "/" + file[1] + "_" + file[0])
+
+        dirs = []
+        for line in system:
+            # get all files in the same directory and remove them from the system
+            # get path till last /
+            path = line[:line.rfind("/")]
+            files = [file for file in system if file.startswith(path) and file.count("/") == line.count("/")]
+            system = [file for file in system if not file.startswith(path) or file.count("/") != line.count("/")]
+            if files:
+                dirs.append(files)
+
+        dir = {}
+        for files in dirs:
+            sum = 0
+            for line in files:
+                # get the file size
+                size = int(line.split("_")[1])
+                # add the file size to the sum
+                sum += size
+            # add the sum to the directory
+            path = files[0][:line.rfind("/")]
+            dir[path] = sum
+            for i in range(path.count("/")):
+                path = path[:path.rfind("/")]
+                if path in dir:
+                    dir[path] += sum
+                else:
+                    dir[path] = sum    
+        # sum all in dir smaller than 100000
+        sum = 0
+        for key, value in dir.items():
+            if value < 100000:
+                sum += value
+                print(key, value)
+        print(sum)
+
+      
+
+if __name__ == '__main__':
+    main()
